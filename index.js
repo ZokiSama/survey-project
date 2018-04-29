81f56d323e03b726a02b7298ee361b39860c32dd
Joakim Hall <joakim.hall1@gmail.com>
2018-04-29 13:47:36 +0200

    console.log on accessToken

--- index.js
+++ index.js
@@ -1,23 +1,21 @@
 const express = require('express');
 const passport = require('passport'); // Hur vi använder authensiering
 const GoogleStrategy = require('passport-google-oauth20').Strategy; // instruerar passport hur användare ska hanteras med google
+const keys = require('./config/keys'); // Path till nycklarnas
 
 const app = express();
-const keys = require('./config/keys');
+
 
 // Skapar en ny instans av GoogleStrategy
-passport.use(new GoogleStrategy({
-  clientID: keys.googleClientID,//keys.js
-  clientSecret: keys.googleClientSecret,//keys.js
-  callbackURL: "http://localhost:5000/auth/google/callback"
-},
-function(accessToken, refreshToken, profile, cb) {
-  User.findOrCreate({ googleId: profile.id },
-  function (err, user) {
-    return cb(err, user);
-  });
-}
-)); 
+passport.use(
+  new GoogleStrategy({
+    clientID: keys.googleClientID,// keys.js
+    clientSecret: keys.googleClientSecret,// keys.js
+    callbackURL: "/auth/google/callback" // Vägen användare kommer att skickas till efter de har godkänt info hos google
+  }, (accessToken) => {
+    console.log(accessToken);
+  })
+); 
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
