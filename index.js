18fa80230e03a7611612e5c85db23a2d99e3754f
Joakim Hall <joakim.hall1@gmail.com>
2018-04-29 13:30:05 +0200

    passport module added

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
-const express = require("express");
-const app = express();
+const express = require('express');
+const passport = require('passport'); // Hur vi använder authensiering
+const GoogleStrategy = require('passport-google-oauth20').Strategy; // instruerar passport hur användare ska hanteras med google
 
-app.get("/", (req, res) => {
-  res.send({ test: "Trying stuff out" });
-});
+const app = express();
+const keys = require('./config/keys');
+
+// Skapar en ny instans av GoogleStrategy
+passport.use(new GoogleStrategy({
+  clientID: keys.googleClientID,//keys.js
+  clientSecret: keys.googleClientSecret,//keys.js
+  callbackURL: "http://localhost:5000/auth/google/callback"
+},
+function(accessToken, refreshToken, profile, cb) {
+  User.findOrCreate({ googleId: profile.id },
+  function (err, user) {
+    return cb(err, user);
+  });
+}
+)); 
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
