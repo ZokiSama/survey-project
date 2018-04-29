cc9e20285d2a603fbd4fb1be5d5798966434a102
Joakim Hall <joakim.hall1@gmail.com>
2018-04-30 00:22:12 +0200

    Reformated with Prettier

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const express = require("express");
-const passport = require("passport"); // Hur vi använder authensiering
-const GoogleStrategy = require("passport-google-oauth20").Strategy; // instruerar passport hur användare ska hanteras med google
-const keys = require("./config/keys"); // Path till nycklarnas
+const express = require('express');
+const passport = require('passport'); // Hur vi använder authensiering
+const GoogleStrategy = require('passport-google-oauth20').Strategy; // instruerar passport hur användare ska hanteras med google
+const keys = require('./config/keys'); // Path till nycklarnas
 
 const app = express();
 
@@ -11,7 +11,7 @@ passport.use(
       // Skapar en ny instans av GoogleStrategy
       clientID: keys.googleClientID, // keys.js
       clientSecret: keys.googleClientSecret, // keys.js
-      callbackURL: "/auth/google/callback" // Vägen användare kommer att skickas till efter de har godkänt info hos google
+      callbackURL: '/auth/google/callback' // Vägen användare kommer att skickas till efter de har godkänt info hos google
     },
     (accessToken, refreshToken, profile, done) => {
       console.log('accessToken: ' + accessToken);
@@ -23,13 +23,13 @@ passport.use(
 
 // Route handler
 app.get(
-  "/auth/google", // När en användare kommer via denna URLen skicka vidare till passport
-  passport.authenticate("google", {
-    scope: ["profile", "email"]
+  '/auth/google', // När en användare kommer via denna URLen skicka vidare till passport
+  passport.authenticate('google', {
+    scope: ['profile', 'email']
   })
 );
 
-app.get("/auth/google/callback", passport.authenticate("google")); // Byt kod med google profil
+app.get('/auth/google/callback', passport.authenticate('google')); // Byt kod med google profil
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
