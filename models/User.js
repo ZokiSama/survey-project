ed1611b9bba5d9ed80e7cd9796d4661b1413f14f
Joakim Hall <joakim.hall1@gmail.com>
2018-05-02 17:04:36 +0200

    Displayname added for schema

--- models/User.js
+++ models/User.js
@@ -1,9 +1,10 @@
-const mongoos = require('mongoose');
+const mongoose = require('mongoose');
 const { Schema } = mongoose; // Same as writing const Schema = mongoos.Schema;
 
 // ett schema som förklarar hur en användare ser ut
 const userSchema = new Schema({
-  googleId: String
+  googleId: String,
+  displayName: String
 });
 
 // Skapar en ny kollektion som heter users (om den inte redan finns).
--- services/passport.js
+++ services/passport.js
@@ -1,6 +1,9 @@
 const passport = require('passport'); // Hur vi använder authensiering
 const GoogleStrategy = require('passport-google-oauth20').Strategy; // instruerar passport hur användare ska hanteras med google
+const mongoose = require('mongoose');
 const keys = require('../config/keys'); // Path till nycklarna
+
+const User = mongoose.model('users'); // Få tillgång till User model class
 
 passport.use(
   new GoogleStrategy(
@@ -11,9 +14,11 @@ passport.use(
       callbackURL: '/auth/google/callback' // Vägen användare kommer att skickas till efter de har godkänt info hos google
     },
     (accessToken, refreshToken, profile, done) => {
-      console.log('accessToken: ' + accessToken);
-      console.log('refreshToken: ' + refreshToken);
-      console.log('profile: ' + JSON.stringify(profile));
+      new User({
+        googleId: profile.id,
+        displayName: profile.displayName
+      }).save(); // Skapar en ny instans av en användare och sparar till MongoDB
+      console.log(profile);
     }
   )
 );
