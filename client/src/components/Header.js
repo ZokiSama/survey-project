b117c01057eef88542bf151a6b1d10b870552868
Joakim Hall <joakim.hall1@gmail.com>
2018-05-14 09:41:05 +0200

    Added style to header(nav bar)

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -2,7 +2,18 @@ import React, { Component } from 'react';
 
 class Header extends Component {
   render() {
-    return <div>Header</div>;
+    return (
+      <nav>
+        <div className="nav-wrapper deep-purple darken-4">
+          <a className="brand-logo">Zottik Mail Surveys</a>
+          <ul className="right">
+            <li>
+              <a>Sign-in with Google</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    );
   }
 }
 
