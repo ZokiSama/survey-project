bdd4f4bfb778aec64db8e3a66ab1dfb5237cb5a1
Joakim Hall <joakim.hall1@gmail.com>
2018-05-14 16:36:19 +0200

    Login and redirect are added to client

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,20 +1,40 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class Header extends Component {
+  renderContent() {
+    switch (this.props.auth) {
+      case null:
+        return 'Waiting';
+      case false:
+        return (
+          <li>
+            <a href="/auth/google">Sign-in with Google</a>
+          </li>
+        );
+      default:
+        return (
+          <li>
+            <a>Logout</a>
+          </li>
+        );
+    }
+  }
+
   render() {
     return (
       <nav>
         <div className="nav-wrapper deep-purple darken-4">
           <a className="brand-logo">Zottik Mail Surveys</a>
-          <ul className="right">
-            <li>
-              <a>Sign-in with Google</a>
-            </li>
-          </ul>
+          <ul className="right">{this.renderContent()}</ul>
         </div>
       </nav>
     );
   }
 }
 
-export default Header;
+function mapStateToProps({ auth }) {
+  return { auth };
+}
+
+export default connect(mapStateToProps)(Header);
