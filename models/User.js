96644802dae1e535986831188c855954fabc3602
Joakim Hall <joakim.hall1@gmail.com>
2018-05-15 15:09:48 +0200

    updating credits after charge. -give error if not logged in

--- models/User.js
+++ models/User.js
@@ -4,7 +4,7 @@ const { Schema } = mongoose; // Same as writing const Schema = mongoos.Schema;
 // ett schema som förklarar hur en användare ser ut
 const userSchema = new Schema({
   googleId: String,
-  displayName: String
+  credits: { type: Number, default: 0 }
 });
 
 // Skapar en ny kollektion som heter users (om den inte redan finns).
