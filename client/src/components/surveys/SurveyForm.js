2d05b5538ba425ddb297bdb075a131a78d4629c3
Joakim Hall <joakim.hall1@gmail.com>
2018-06-03 18:38:10 +0200

    email check

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -4,6 +4,7 @@ import React, { Component } from 'react';
 import { reduxForm, Field } from 'redux-form';
 import { Link } from 'react-router-dom';
 import SurveyField from './SurveyField';
+import validateEmails from '../../utils/validateEmails';
 
 const FIELDS = [
   {
@@ -62,6 +63,8 @@ class SurveyForm extends Component {
 
 function validate(values) {
   const errors = {};
+
+  errors.emails = validateEmails(values.emails || '');
 
   _.each(FIELDS, ({ name, noValueErr }) => {
     if (!values[name]) {
